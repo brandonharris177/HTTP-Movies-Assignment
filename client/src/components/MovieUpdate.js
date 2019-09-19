b08ac0c937098bd8ec9b65f4d1ddb2a8523efcc4
Brandon-Harris <brandonharris177@gmail.com>
2019-09-19 14:35:53 -0600

    update form built

--- client/src/components/MovieUpdate.js
+++ client/src/components/MovieUpdate.js
@@ -1,7 +1,13 @@
 import React, {useState, useEffect} from 'react';
+import Movie from '../Movies/Movie';
 
 const MovieUpdate = (props) => {
-   const [updatedMovie, setUpdatedMovie] = useState([])
+   const [updatedMovie, setUpdatedMovie] = useState({
+        title: "",
+        director: "",
+        metascore: "",
+        stars: [] 
+   })
   
     // useEffect = () => {
     //   axios
@@ -9,9 +15,39 @@ const MovieUpdate = (props) => {
     //     .then(res => this.setState({ movies: res.data }))
     //     .catch(err => console.log(err.response));
     // }
+
+    const handleChange = event => {
+        setUpdatedMovie({...updatedMovie, [event.target.name]: event.target.value })
+    }
   
     return (
     <>
+        <form>
+            <input 
+                type = 'text'
+                name = 'title'
+                placeholder = 'Movie Title'
+                value = {updatedMovie.title}
+                onChange = {handleChange}/>
+            <input 
+                type = 'text'
+                name = 'director'
+                placeholder = 'Director'
+                value = {updatedMovie.director}
+                onChange = {handleChange}/>
+            <input 
+                type = 'number'
+                name = 'metascore'
+                placeholder = 'Metascore'
+                value = {updatedMovie.metascore}
+                onChange = {handleChange}/>
+            <input 
+                type = 'text'
+                name = 'stars'
+                placeholder = 'Stars'
+                value = {updatedMovie.stars}
+                onChange = {handleChange}/>
+        </form>
 
     </>
     );
