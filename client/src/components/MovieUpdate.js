e2d8bacfef1a86da0bbf77937de152da6de4b442
Brandon-Harris <brandonharris177@gmail.com>
2019-09-19 13:50:20 -0600

    building out update component

---
+++ client/src/components/MovieUpdate.js
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from 'react';
+
+const MovieUpdate = (props) => {
+    [updatedMovie, setUpdatedMovie] = useState ([])
+  
+    // useEffect = () => {
+    //   axios
+    //     .put("http://localhost:5000/api/movies")
+    //     .then(res => this.setState({ movies: res.data }))
+    //     .catch(err => console.log(err.response));
+    // }
+  
+    return (
+    <div>
+    </div>
+    );
+}
+
+  export default MovieUpdate
+  
